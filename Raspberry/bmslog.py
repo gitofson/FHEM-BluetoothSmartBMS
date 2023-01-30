7f4d4b91b6df5cae9ac9ed24c778e00835ca4a22
gitofson <stemberk@gmail.com>
2023-01-30 07:56:54 +0000

    fix

--- Raspberry/bmslog.py
+++ Raspberry/bmslog.py
@@ -4,28 +4,10 @@
 import gatt
 import json
 import sys
-from influxdb import InfluxDBClient
+from db import db
 
 
 manager = gatt.DeviceManager(adapter_name='hci0')
-class db:
-    dbname = ['pv_main']
-    @staticmethod
-    def dbsave(measurement, rawdat):
-        mlist=db.dbname+[measurement]
-        json_body = [
-                    {
-                    "measurement": measurement,
-                    "fields": rawdat
-                    }
-        ]
-        client = InfluxDBClient(host='elektrovit.cz', port=8086)
-        for dbname in mlist:
-            #client.create_database(dbname)
-            client.switch_database(dbname)
-            client.write_points(json_body)
-
-
 class AnyDevice(gatt.Device):
     def connect_succeeded(self):
         super().connect_succeeded()
--- Raspberry/db.py
+++ Raspberry/db.py
@@ -3,18 +3,15 @@ from influxdb import InfluxDBClient
 class db:
     dbname = ['pv_main']
     @staticmethod
-    def dbsave(measurement, rawdat):
+    def dbsave(measurement, rawdat, defSeriesName='brushEvents'):
         mlist=db.dbname+[measurement]
-        json_body = [
-                    {
-                    "measurement": measurement,
-                    "fields": rawdat
-                    }
-        ]
         client = InfluxDBClient(host='elektrovit.cz', port=8086)
         for dbname in mlist:
             #client.create_database(dbname)
             client.switch_database(dbname)
-            client.write_points(json_body)
-
-
+            client.write_points([
+                    {
+                    "measurement": measurement if dbname == 'pv_main' else defSeriesName,
+                    "fields": rawdat
+                    }
+            ])
